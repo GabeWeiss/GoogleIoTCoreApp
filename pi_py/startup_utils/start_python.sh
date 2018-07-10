1c92d7e3c5641796b094f999f3b277986284b54c
Calum Barnes <calumb@gmail.com>
2018-07-10 01:17:58 -0400

    draft bash script - to be tested on pi

--- pi_py/startup_utils/start_python.sh
+++ pi_py/startup_utils/start_python.sh
@@ -8,5 +8,7 @@ source bin/activate
 
 pip install -r requirements.txt
 
-
-python bpm_writer.py > /home/pi/bpm.log 2>&1 &
+if [ $FAKE = true ] ; then
+	python bpm_fake.py > /home/pi/bpm_fake.log 2>&1 &
+fi
+	python bpm_writer.py > /home/pi/bpm.log 2>&1 &	
