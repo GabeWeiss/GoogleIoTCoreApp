68b789a4e7925f428fd8223c6bb73e7a08ed79d7
Calum Barnes <calumb@gmail.com>
2018-07-11 00:20:53 -0400

    remove pip option

--- pi_py/startup_utils/start_python.sh
+++ pi_py/startup_utils/start_python.sh
@@ -3,6 +3,6 @@
 cd /home/pi/bpm_run/GoogleIoTCoreApp/pi_py/
 git pull
 
-pip install -r requirements.txt --ignore-installed
+pip install -r requirements.txt
 
 python -u bpm_writer.py > /home/pi/bpm.log 2>&1 &
