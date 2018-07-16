faf94baa19b15a848a6d55ec678dcfc760df66c5
Calum Barnes <calumb@gmail.com>
2018-07-16 13:57:31 -0400

    no pip install

--- pi_py/startup_utils/run_pip.sh
+++ pi_py/startup_utils/run_pip.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #uncomment to run pip - should only be needed when requirements.txt has changed
-/usr/bin/pip install -r /home/pi/bpm_run/GoogleIoTCoreApp/pi_py/requirements.txt
+#/usr/bin/pip install -r /home/pi/bpm_run/GoogleIoTCoreApp/pi_py/requirements.txt
